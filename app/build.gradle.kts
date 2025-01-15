333020d69ca7e884b8abd1843514b3dd649cfeb4
Liher27 <liher.chamorropa@elorrieta-errekamari.com>
2025-01-15 20:25:34 +0100

    Terminado panel de login, inicio del panel de registro

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.reto2_grupo2"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.reto2_grupo2"
