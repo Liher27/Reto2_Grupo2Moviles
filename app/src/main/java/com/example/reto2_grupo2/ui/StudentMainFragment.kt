69e8396669d4f818ccd5430cbb956f93f0d40853
Liher27 <liher.chamorropa@elorrieta-errekamari.com>
2025-01-18 17:04:15 +0100

    Añadido limite de fechas del calendario, y añadida caract. de añadir fotos

--- app/src/main/java/com/example/reto2_grupo2/ui/StudentMainFragment.kt
+++ app/src/main/java/com/example/reto2_grupo2/ui/StudentMainFragment.kt
@@ -1,26 +1,19 @@
 package com.example.reto2_grupo2.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CalendarView
+import androidx.fragment.app.Fragment
 import com.example.reto2_grupo2.R
+import java.util.Calendar
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [StudentMainFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class StudentMainFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var calendarView: CalendarView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,19 +28,32 @@ class StudentMainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.student_main_fragment, container, false)
+        val view = inflater.inflate(R.layout.student_main_fragment, container, false)
+        calendarView = view.findViewById(R.id.calendarView)
+        return view
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+        val calendar = Calendar.getInstance()
+
+        calendar.add(Calendar.MONTH, -4)
+        val minDate = calendar.timeInMillis
+        calendarView.minDate = minDate
+
+        calendar.add(Calendar.MONTH, 8)
+        calendar.add(Calendar.DAY_OF_MONTH, 13)
+        val maxDate = calendar.timeInMillis
+        calendarView.maxDate = maxDate
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment StudentMainFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+        private const val ARG_PARAM1 = "param1"
+        private const val ARG_PARAM2 = "param2"
+
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             StudentMainFragment().apply {
