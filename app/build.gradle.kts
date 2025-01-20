19ee5424a871bd2519981bc10830eec9d0bf56be
Liher27 <liher.chamorropa@elorrieta-errekamari.com>
2025-01-20 19:44:37 +0100

    actualizada la vista de los cursos externos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.google.maps.sdk)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.fragment.ktx)
